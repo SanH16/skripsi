03b9f09a77722b37965bcdc4b97b5954214a89a6
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2024-06-18 16:08:09 +0700

    add some change layout and rekrutmen

--- client/src/routers/index.jsx
+++ client/src/routers/index.jsx
@@ -2,12 +2,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Notfound from "@/views/error-views/Notfound";
 import LandingPage from "@/views/landing-views";
 import AppLayout from "@/components/layout-components/AppLayout";
+import Rekrutmen from "@/views/app-views/rekrutmen";
 
 export default function SetupRoutes() {
   return (
     <Routes>
       <Route index element={<LandingPage />} />
       <Route path="/dashboard" element={<AppLayout />} />
+      <Route path="/rekrutmen" element={<Rekrutmen />} />
       <Route>
         <Route path="/404" element={<Notfound />} />
         <Route path="*" element={<Navigate to="/404" replace />} />
