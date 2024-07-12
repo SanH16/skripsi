47f03335ebdcb3c0fb4220f1efd6578495a13db9
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2024-07-12 16:45:08 +0700

    add new change schema penugasan also slcing ui penugasan

---
+++ server/models/PenugasanModel.js
@@ -0,0 +1,74 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+import Users from "./UserModel.js";
+
+const { DataTypes } = Sequelize;
+
+const Penugasan = db.define(
+  "penugasan",
+  {
+    uuid: {
+      type: DataTypes.STRING,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    judul: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    keterangan_tugas: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    target_selesai: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    status_tugas: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    tasks_list: {
+      type: DataTypes.JSON,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    completed_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+      references: {
+        model: Users,
+        key: "id",
+      },
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+Users.hasMany(Penugasan, { foreignKey: "userId" });
+Penugasan.belongsTo(Users, { foreignKey: "userId" });
+
+export default Penugasan;
